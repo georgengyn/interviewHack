e17e85a220ca21708bfed4e843fc9647a58ee335
georgengyn <g.nguyen@wustl.edu>
2024-10-19 18:38:07 -0500

    html file

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,13 @@
 from flask import Flask
+from flask import render_template
+import os
 
 app = Flask(__name__)
 
 @app.route("/")
-def welcome():
-    return "<p>Hello, World</p>"
+@app.route("/home")
+def welcome(): 
+    return render_template('home.html')
 
 if __name__ == '__main__':
     app.run()
