2282b0fc082d3a7293ff1267d4dbf0c271370481
georgengyn <g.nguyen@wustl.edu>
2024-10-19 18:56:18 -0500

    change

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,10 @@ import os
 
 app = Flask(__name__)
 
+UPLOAD_FOLDER = 'uploads'
+if not os.path.exists(UPLOAD_FOLDER):
+    os.makedirs(UPLOAD_FOLDER)
+
 @app.route("/")
 @app.route("/home")
 def welcome(): 
